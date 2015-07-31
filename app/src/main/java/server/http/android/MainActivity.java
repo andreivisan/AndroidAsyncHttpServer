21a9ccb6be9cb07f2d63776606935964afecf087
Andrei Visan <andrei.m.visan@gmail.com>
2015-07-31 15:39:55 +0200

    First commit

---
+++ app/src/main/java/server/http/android/MainActivity.java
@@ -0,0 +1,67 @@
+package server.http.android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.koushikdutta.async.AsyncServer;
+import com.koushikdutta.async.http.server.AsyncHttpServer;
+import com.koushikdutta.async.http.server.AsyncHttpServerRequest;
+import com.koushikdutta.async.http.server.AsyncHttpServerResponse;
+import com.koushikdutta.async.http.server.HttpServerRequestCallback;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private AsyncHttpServer server = new AsyncHttpServer();
+    private AsyncServer mAsyncServer = new AsyncServer();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        startServer();
+    }
+
+//    @Override
+//    public void onPause() {
+//        super.onPause();
+//        //server.stop();
+//    }
+
+    private void startServer() {
+        Log.i("async", "=======> server started");
+
+        server.get("/", new HttpServerRequestCallback() {
+            @Override
+            public void onRequest(AsyncHttpServerRequest request, AsyncHttpServerResponse response) {
+                Log.i("async", "=======> server started and request served");
+                response.send("Hello!!!");
+            }
+        });
+
+        server.listen(mAsyncServer, 8080);
+    }
+}
